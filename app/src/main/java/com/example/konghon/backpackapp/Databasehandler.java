02243274e8a21d36b69e32b5d6acec058f30b049
Konghon <konghon@live.nl>
2014-10-31 10:48:59 +0100

    added database handles class

---
+++ app/src/main/java/com/example/konghon/backpackapp/Databasehandler.java
@@ -0,0 +1,30 @@
+package com.example.konghon.backpackapp;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+/**
+ * Created by Konghon on 31/10/2014.
+ */
+public class Databasehandler extends SQLiteOpenHelper
+{
+    private static final int DATABASE_VERSION = 1; 				// Database Version
+    private static final String DATABASE_NAME = "Backpack"; 	// Database Name
+    private static final String TABLE_MESSAGES = "messages";	// Contacts table name
+
+    public Databasehandler(Context context)
+    {
+        super(context, DATABASE_NAME, null, DATABASE_VERSION);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion){
+
+    }
+
+}
